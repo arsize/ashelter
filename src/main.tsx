e56c7fc355a7e1b7578244e81894f033af12fe79
arsize <arsizeof3416@gmail.com>
2023-05-16 10:58:23 +0800

    fix:bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "@/styles/global.css"
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import '@/styles/global.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
 )
